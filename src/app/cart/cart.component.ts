8451b8b1b869eb30ea6515666b0dd61574159e5b
Dmitry Vnukevich <dmitryvnukevich@dswinc.com>
2018-06-29 13:06:20 +0300

    add total to cart page

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -4,7 +4,12 @@ import {CartService} from 'tsl-commerce-services-a2/dist/services';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent {
   constructor(public cartService: CartService) {}
+
+  handleCheckout() {
+    alert('Start checkout process...');
+  }
 }
